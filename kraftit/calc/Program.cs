968961cc1542d2df76435287150fd25393a67704
Denis <47280607+Denok137@users.noreply.github.com>
2019-02-07 15:55:08 +0500

    Add files via upload

---
+++ kraftit/calc/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Program
+    {
+        public static Dictionary<string, int> variables = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                List<string> data = new List<string>(); // части выражения
+                List<char> rawData; //введенные символы
+
+                string line;
+                int result = 0;
+
+                line = Console.ReadLine();
+
+                rawData = new List<char>(line); //представляем выражение в символах
+                rawData.RemoveAll(ch => ch == ' '); //не зависим от пробелов в записи
+
+                data = ConvertData(data, rawData); //превращаем символы в части выражения
+
+                result = Calculate(data); //считаем
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static List<string> ConvertData(List<string> data, List<char> rawData)
+        {
+            string item;
+            int nextIndex;
+            for (int i = 0; i < rawData.Count; i++) //перебираем символы
+            {
+                item = ""; //сюда будем записывать часть выражения
+
+                do
+                {
+                    if (rawData[i] == '+' || rawData[i] == '-')
+                    { //знаки входят в операнды
+                        item += rawData[i];
+                        nextIndex = i++;
+                    }
+
+                    item += rawData[i];
+                    nextIndex = i++;
+                    if (nextIndex + 1 == rawData.Count) break;
+                }
+                while (Char.GetUnicodeCategory(rawData[i]) == Char.GetUnicodeCategory(rawData[nextIndex])); //пока символы одного типа они складываются в часть выражения
+
+
+                data.Add(item);
+
+                i -= 1; //возвращаемся к символу перед разделителем
+            }
+            return data;
+        }
+
+        public static int Calculate(List<string> data)
+        {
+            int result = 0;
+            var equally = data.IndexOf("=");
+            if (equally > 0) //если присвоение
+            {
+                //правая часть 
+                for (int i = equally + 1; i < data.Count; i++) //перебираем операнды. используем индекс = чтоб найти откуда начинать
+                {
+                    result += GetNumber(data[i]); //считаем
+                }
+
+                //левая часть
+                if (variables.ContainsKey(data[0])) //если есть в словаре перезаписываем
+                {
+                    variables[data[0]] = result;
+                }
+                else //если нету добавляем
+                {
+                    variables.Add(data[0], result);
+                }
+
+            }
+            else
+            { //если просто операция
+                for (int i = 0; i < data.Count; i++)
+                {
+                    result += GetNumber(data[i]); //складываем все
+                }
+            }
+
+            return result;
+        }
+
+        public static int GetNumber(string op) //превращаем операнды в цифры
+        {
+            int number = 0;
+            bool minus = false;
+
+            if (op.Any(char.IsLetter)) //если переменная
+            {
+                if (op.Contains('-')) minus = true; //запоминаем что там был минус
+                op = op.Trim('-', '+'); //очищаем от знаков чтоб найти в словаре
+
+                try
+                {
+                    number = variables[op]; //берем из словаря
+                    if (minus) number = number * -1;
+                }
+                catch { Console.WriteLine("Попытка использования неинициализированной переменной"); }
+
+            }
+            else //если просто число
+            {
+                number = Convert.ToInt32(op);
+            }
+            return number;
+        }
+
+    }
+}
---
+++ kraftit/queue/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            List<int> tasksList = new List<int>();
+
+            List<Server> servers = new List<Server> {
+                new Server { name = "1" },
+                new Server { name = "2" },
+                new Server { name = "3" },
+             };
+
+            while (true)
+            {
+                line = Console.ReadLine();
+
+                try
+                {
+                    tasksList = line.Split(',').Select(int.Parse).ToList();
+                    tasksList = tasksList.OrderByDescending(item => item).ToList();
+                }
+                catch { Console.WriteLine("Введенная строка не соотвествует формату -  1,2,3,4 "); }
+
+                //распределяем
+                SetTasks(tasksList, servers);
+
+                //выводим на экран
+                Print(servers);
+            }
+
+        }
+
+        private static void SetTasks(List<int> tasksList, List<Server> servers)
+        {
+            Server lazyserver = new Server();
+
+            int min;
+            for (int i = 0; i < tasksList.Count(); i++) //раздаем задачи
+            {
+                lazyserver = null;
+                min = int.MaxValue;
+
+                for (int j = 0; j < servers.Count; j++) //ищем самый незагруженный сервер
+                {
+
+                    if (servers[j].GetWorkTime() < min)
+                    {
+                        min = servers[j].GetWorkTime(); lazyserver = servers[j];
+                    }
+
+                }
+
+                lazyserver.AddTask(tasksList[i]); //даем задачу
+            }
+        }
+
+        private static void Print(List<Server> servers)
+        {
+            string serverTasks = "";
+
+            foreach (var server in servers)
+            {
+                serverTasks = "";
+
+                foreach (var taskTime in server.Tasks)
+                {
+                    serverTasks += "|" + new string('_', taskTime) + "|";
+                }
+
+                Console.WriteLine(server.name + ":" + serverTasks);
+
+            }
+        }
+    }
+
+    public class Server {
+        public string name;
+
+        public List<int> Tasks { get; set; }
+
+        public void AddTask(int task)
+        {
+            Tasks.Add(task);
+        }
+  
+        public int GetWorkTime() {
+            int time = 0;
+
+            foreach (var task in Tasks) {
+                time += task;
+            }
+            return time;
+        }
+
+        public Server()
+        {
+            Tasks = new List<int>();
+        }
+
+    }
+
+}
